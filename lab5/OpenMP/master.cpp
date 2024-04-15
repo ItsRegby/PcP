94e1ff536f6a02e6d8151ca59a3bb17044cccbba
ItsRegby <shevchenko.eduard1121@vu.cdu.edu.ua>
2024-04-15 23:07:53 +0300

    Update master.cpp

--- lab5/OpenMP/master.cpp
+++ lab5/OpenMP/master.cpp
@@ -4,17 +4,21 @@
 int main(int argc, char** argv)
 {
     int master_v = 100;
+	int id = 0;
 #pragma omp parallel
     {
         int v = 0;
-#pragma omp master
-	{
-	    v = master_v;
+		#pragma omp master
+		{
+		    v = master_v;
+			#pragma omp critical
+				printf("master thread, v = %d\n", v);
+		}
 		#pragma omp critical
-	    std::cout << "master thread, v = "<< v << std::endl;
-	}
-#pragma omp critical
-	std::cout << "v = "<< v  << std::endl;
+		{
+			id = omp_get_thread_num();
+			printf("v = %d  id = %d\n", v, id);
+		}
     } 
     return 0;
 }
